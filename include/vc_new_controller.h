cbca5b5d1b65a15d84c99167d95702b5a8cdaa26
Leonardo Ramirez <deiividramirez@gmail.com>
2023-01-07 11:06:53 -0600

    Modif - ArUco behind drone

--- include/vc_new_controller.h
+++ include/vc_new_controller.h
@@ -1,31 +1,30 @@
 #ifndef VC_CONTROLLER_H
 #define VC_CONTROLLER_H
 
-
 /******************************************************* ROS libraries*/
 #include <ros/ros.h>
-#include "tf/transform_datatypes.h"
-#include <trajectory_msgs/MultiDOFJointTrajectory.h>
 #include <mav_msgs/conversions.h>
-#include <image_transport/image_transport.h>
+#include "tf/transform_datatypes.h"
 #include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
+#include <trajectory_msgs/MultiDOFJointTrajectory.h>
 
 /**************************************************** OpenCv Libraries*/
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
-#include <cv_bridge/cv_bridge.h>
-#include <opencv2/features2d.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/calib3d.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/features2d.hpp>
 
 /*************************************************** c++ libraries */
-#include <iostream>
-#include <fstream>
-#include <stdlib.h>
-#include <stdio.h>
-#include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <iostream>
 
 /*************************************************** custom libraries */
 #include "vc_state/vc_state.h"
